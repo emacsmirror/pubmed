161b2c64e7a0b73533fb28a89d861026e797947a
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2019-03-13 20:35:50 +0100

    Added pubmed-pkg.el

---
+++ pubmed-pkg.el
@@ -0,0 +1,10 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pubmed" "0.1" "Interface to PubMed"
+  '((emacs "25.1")
+    (deferred "20170901.1330")
+    (esxml "20171129.807")
+    (s "20180406.808"))
+  :keywords '("pubmed")
+  :authors '(("Folkert van der Beek" . "folkertvanderbeek@xs4all.nl"))
+  :maintainer '("Folkert van der Beek" . "folkertvanderbeek@xs4all.nl")
+  :url "https://gitlab.com/fvdbeek/emacs-pubmed")
