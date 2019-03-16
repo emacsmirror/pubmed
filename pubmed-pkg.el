408bfc7b4b9f0e1cf3be6191579c6fbe76d0873b
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2019-03-16 21:02:43 +0100

    Updated package requires

--- pubmed-pkg.el
+++ pubmed-pkg.el
@@ -1,9 +1,9 @@
 ;;; -*- no-byte-compile: t -*-
 (define-package "pubmed" "0.1" "Interface to PubMed"
   '((emacs "25.1")
-    (deferred "20170901.1330")
+    (deferred "0.5.1")
     (esxml "20171129.807")
-    (s "20180406.808"))
+    (s "1.12.0"))
   :keywords '("pubmed")
   :authors '(("Folkert van der Beek" . "folkertvanderbeek@gmail.com"))
   :maintainer '("Folkert van der Beek" . "folkertvanderbeek@gmail.com")
