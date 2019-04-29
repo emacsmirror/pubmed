870c470bea26bfcc6bfe9230c701e97c17eca79e
Folkert van der Beek <folkertvanderbeek@xs4all.nl>
2019-04-29 02:10:43 +0200

    Updated version number

--- pubmed.el
+++ pubmed.el
@@ -2,7 +2,7 @@
 
 ;; Author: Folkert van der Beek <folkertvanderbeek@gmail.com>
 ;; Created: 2018-05-23
-;; Version: 0.1
+;; Version: 0.2
 ;; Keywords: pubmed, hypermedia
 ;; Package-Requires: ((emacs "25.1") (deferred "0.5.1") (esxml "0.3.4") (s "1.12.0"))
 ;; URL: https://gitlab.com/fvdbeek/emacs-pubmed
